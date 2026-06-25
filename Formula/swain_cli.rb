d959ca67a64260cb918b9fcf775884a1db3b4d56
takifouhal <37051494+takifouhal@users.noreply.github.com>
2026-06-25 12:59:02 +0100

    chore(release): bump Homebrew formula to v0.3.19

--- Formula/swain_cli.rb
+++ Formula/swain_cli.rb
@@ -6,21 +6,21 @@ class SwainCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.18/swain_cli-macos-arm64"
-      sha256 "2dfc303aebd0193b50354bd2cf6183be507cc3c576e8fc29d1e318b8c4b34023"
+      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.19/swain_cli-macos-arm64"
+      sha256 "289fe215b8e806e5539d6ef7b194a4756472930ae47a7a75e9d1f2d4d1fb3fa5"
     else
-      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.18/swain_cli-macos-x86_64"
-      sha256 "05be7fb09acb1f91d5d417be2f933c76636b994dff38241a3aed4e66286fe1f1"
+      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.19/swain_cli-macos-x86_64"
+      sha256 "95cec6958ac27d2100132e43735a2bdec74e785c564c5d6123030c177ccf2d59"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.18/swain_cli-linux-arm64"
-      sha256 "54139f7f90a087b910b8b36d301db1196712e809f7afcf3a34cc1fd2db7302f5"
+      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.19/swain_cli-linux-arm64"
+      sha256 "1bcb9f6a48ab4431afb32080b712968efbc6c7e16c81a023ede85c5fddab5363"
     else
-      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.18/swain_cli-linux-x86_64"
-      sha256 "d9e1176db59ae96f403d4254670b0a8e3e2a9fcb5c0e8b4a4729b166c9bad8e4"
+      url "https://github.com/takifouhal/swain_cli/releases/download/v0.3.19/swain_cli-linux-x86_64"
+      sha256 "8e0cedc3ece6e4fb941bd8cc097c28470c15b564412280356e34acfa8f07cc4b"
     end
   end
 
